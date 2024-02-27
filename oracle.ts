4a877498ca0a8016fc14247b0f273fa54918fbaf
Igor Sikachyna <igor.sikachyna@ultra.io>
2024-02-27 08:22:16 -0500

    Updated package.json

--- oracle.ts
+++ oracle.ts
@@ -8,6 +8,16 @@ import { logger } from '../../../utility';
 import { SystemAPI, system } from '../../system';
 import { UltraAPIv2 } from '../../ultraStartup';
 
+// import { HTTP_API } from '@ultraos/ultratest/src/apis/http';
+// import * as api from '@ultraos/ultratest/src/apis/pluginApi';
+// import { Plugin, argsToParams } from '@ultraos/ultratest/src/interfaces/plugin';
+// import { UltraTest, UltraTestAPI } from '@ultraos/ultratest/src/interfaces/test';
+// import { config, keychain } from '@ultraos/ultratest/src/services';
+// import { logger } from '@ultraos/ultratest/src/utility';
+
+// import { SystemAPI, system } from '@ultraos/ultratest/src/plugins/system';
+// import { UltraAPIv2 } from '@ultraos/ultratest/src/plugins/ultraStartup';
+
 export class OracleAPI {
     public logger: typeof logger;
     private api: HTTP_API;
